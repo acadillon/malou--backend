c515852db71b2ff410e957f6d75e8bac9adff246
Alice Cadillon <acadillon@gmail.com>
2025-04-03 15:48:01 +0200

    creation poems preview fields, creation homepage contentn type

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,3 +1,35 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface PoemsLine extends Struct.ComponentSchema {
+  collectionName: 'components_poems_lines';
+  info: {
+    description: '';
+    displayName: 'Line';
+    icon: 'feather';
+  };
+  attributes: {
+    line: Schema.Attribute.String;
+  };
+}
+
+export interface PoemsPoem extends Struct.ComponentSchema {
+  collectionName: 'components_poems_poems';
+  info: {
+    description: '';
+    displayName: 'Poem';
+    icon: 'handHeart';
+  };
+  attributes: {
+    defaultTitle: Schema.Attribute.String & Schema.Attribute.DefaultTo<'poem'>;
+    Lines: Schema.Attribute.Component<'poems.line', true>;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'poems.line': PoemsLine;
+      'poems.poem': PoemsPoem;
+    }
+  }
+}
